f05a0d3b511d40da5403e5dbfad4c2771c12a927
corentinclement <corentinclement@pm.me>
2021-08-30 09:58:25 +0200

    Exo 3 - map to disc

--- exo/list/Disc.java
+++ exo/list/Disc.java
@@ -1,14 +1,23 @@
 package exo.list;
 
+import java.util.Objects;
+import java.util.UUID;
+
 public class Disc {
+    private int id;
     private String name;
     private int year;
     private Author author;
 
-    public Disc(String name, int year, Author author) {
+    public Disc(int id, String name, int year, Author author) {
+        this.id = id;
         this.name = name;
         this.year = year;
         this.author = author;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -25,5 +34,18 @@ public class Disc {
 
     public void displayInfo() {
         System.out.println(author + " a sorti '" + name + "' en " + year);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Disc disc = (Disc) o;
+        return id == disc.id && year == disc.year && Objects.equals(name, disc.name) && Objects.equals(author, disc.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, year, author);
     }
 }
--- exo/list/Main.java
+++ exo/list/Main.java
@@ -1,7 +1,6 @@
 package exo.list;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -10,9 +9,9 @@ public class Main {
         Author amyWinehouse = new Author("Amy Winehouse");
         Author davidBowie = new Author("David Bowie");
 
-        Disc darkSideOfTheMoon = new Disc("Dark side of the moon", 1973, pinkFloyd);
-        Disc backToBlack = new Disc("Back to black", 2005, amyWinehouse);
-        Disc alladinSane = new Disc("Alladin Sane", 1973, davidBowie);
+        Disc darkSideOfTheMoon = new Disc(1, "Dark side of the moon", 1973, pinkFloyd);
+        Disc backToBlack = new Disc(2, "Back to black", 2005, amyWinehouse);
+        Disc alladinSane = new Disc(3, "Alladin Sane", 1973, davidBowie);
 
         List<Disc> myDiscs = new ArrayList<>();
         myDiscs.add(darkSideOfTheMoon);
@@ -21,8 +20,29 @@ public class Main {
 
         searchByReleaseYear(myDiscs, 1973)
                 .forEach(Disc::displayInfo);
-
         searchByAuthorName(myDiscs, "David Bowie").forEach(Disc::displayInfo);
+
+        Map<Integer, Disc> mapFromDiscList = createMapFromDiscList(myDiscs);
+        System.out.println("Seasch disc with id=3");
+        findByDiscId(mapFromDiscList, 3)
+                .displayInfo();
+    }
+
+    public static Map<Integer, Disc> createMapFromDiscList(List<Disc> discList) {
+        // old way - java 7
+        Map<Integer, Disc> result = new HashMap<>();
+        for (Disc disc : discList) {
+            result.put(disc.getId(), disc);
+        }
+
+        // new way - java 8
+        return discList
+                .stream()
+                .collect(Collectors.toMap(Disc::getId, disc -> disc));
+    }
+
+    public static Disc findByDiscId(Map<Integer, Disc> discMap, int discId) {
+        return discMap.get(discId);
     }
 
     public static List<Disc> searchByReleaseYear(List<Disc> discs, int year) {
